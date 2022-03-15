ee68c2fcabf49ffc01ad607b9753d0cdfcd0702a
Satyajit <paulsjit@gmail.com>
2022-03-15 22:46:51 +0530

    'Login Page Added'

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,12 +2,11 @@ import React from 'react';
 import './Login.css';
 
 function Login() {
-    let price = 10;
-    let VAT = 0.25;
+    
 
     return (
         <div>
-        <h1>Login {`Total: ${(price * (1 + VAT)).toFixed(2)}`}</h1>
+        <h1>Login Its A Login Page</h1>
         </div>
     );
 }
