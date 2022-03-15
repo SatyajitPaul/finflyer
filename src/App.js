08e99c165a4e925fdf82fe780794be7aeea4270b
chaitanya <pchaitanya40@gmail.com>
2022-03-15 23:35:12 +0530

    loginAdded

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import React from 'react';
 import './App.css';
-<<<<<<< HEAD
-import Login from './components/login';
+import Login from './components/login.js';
 function App() {
   return (
     <div className="App">
       <Login />
     </div>
-=======
-import Login from './components/Login';
-
-function App() {
-  return (
-    <Login />
->>>>>>> bb62ffc99e56adf44aea5fa5d9d8e3f1144c418a
   );
 }
 
