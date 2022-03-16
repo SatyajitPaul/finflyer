b4234441d89c631373b46befa65b7919562dacfc
Satyajit Paul <paulsjit@gmail.com>
2022-03-16 17:56:29 +0530

    Error Remove

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React, { useState } from "react";
-import { makeStyles, Grid } from "@material-ui/core";
+import { Grid } from "@material-ui/core";
 import "./App.css";
 import Login from "./components/login.js";
 import Navbar from "./components/Navbar";
 import Leftbar from "./components/Leftbar";
 import Feed from "./components/Feed";
 
-const useStyle = makeStyles((theme) => ({}));
+//const useStyle = makeStyles((theme) => ({}));
 function App() {
 	const [isloogedIn, setIsLoggedIn] = useState(false);
  
   
-	const classes = useStyle();
+	//const classes = useStyle();
 	return (
 		<div className='App'>
 			{isloogedIn ? (
